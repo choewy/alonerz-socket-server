099a28138b58d0a7353cf425a08ac1c0f1af55f7
choewy <70950533+choewy@users.noreply.github.com>
2022-03-04 05:39:46 +0900

    client

---
+++ src/utils/rows.js
@@ -0,0 +1,93 @@
+export const posts = [{
+    post_id: "1",
+    user_id: 'choewy',
+    user_name: "최원영",
+    title: "다섯번째 줄까지만 써본 글",
+    content: "제목 1 포스팅의 내용\n한 줄\n두 줄\n세 줄\n네 줄\n다섯 줄",
+    view_count: 99,
+    tags: ["Spring", "Backend"],
+    createdAt: "2022.12.31(20:20:11)",
+    updatedAt: "2022.12.31(20:21:11)"
+}, {
+    post_id: "2",
+    user_id: 'choewy',
+    title: "ㅁ밖에 없지만 무척 길게 느껴지는 글",
+    content: "제목 2 포스팅의 내용\n긴 글도 가능한가?\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ\nㅁ",
+    writer: "작성자",
+    view_count: 55,
+    tags: ["JS", "React"],
+    createdAt: "2022.12.31.(20:20:11)",
+    updatedAt: "2022.12.31.(20:21:11)"
+}];
+
+export const comments = [{
+    comment_id: "1",
+    user_id: "test",
+    user_name: "테스트",
+    post_id: "1",
+    comment: "우왕 굳\n대박~~",
+    link_count: 33,
+    hate_count: 0,
+    createdAt: "2022.12.31.(20:20:11)",
+    updatedAt: "2022.12.31.(20:21:11)"
+}, {
+    comment_id: "2",
+    user_id: "choewy",
+    user_name: "최원영",
+    post_id: "1",
+    comment: "내 목적은 API 서버 구현인데...",
+    link_count: 12,
+    hate_count: 0,
+    createdAt: "2022.12.31.(20:20:11)",
+    updatedAt: "2022.12.31.(20:20:11)"
+}, {
+    comment_id: "3",
+    user_id: "test",
+    user_name: "테스트",
+    post_id: "1",
+    comment: "배보다 배꼽이 더 큰데?",
+    link_count: 33,
+    hate_count: 0,
+    createdAt: "2022.12.31.(20:20:11)",
+    updatedAt: "2022.12.31.(20:21:11)"
+}, {
+    comment_id: "4",
+    user_id: "test",
+    user_name: "테스트",
+    post_id: "1",
+    comment: "완전 좋은데요!",
+    link_count: 0,
+    hate_count: 0,
+    createdAt: "2022.12.31.(20:20:11)",
+    updatedAt: "2022.12.31.(20:21:11)"
+}, {
+    comment_id: "5",
+    user_id: "test",
+    user_name: "테스트",
+    post_id: "2",
+    comment: "우왕 굳\n대박~~",
+    link_count: 0,
+    hate_count: 0,
+    createdAt: "2022.12.31.(20:20:11)",
+    updatedAt: "2022.12.31.(20:21:11)"
+}, {
+    comment_id: "6",
+    user_id: "test",
+    user_name: "테스트",
+    post_id: "2",
+    comment: "좋은 아이디어네요!",
+    link_count: 0,
+    hate_count: 0,
+    createdAt: "2022.12.31.(20:20:11)",
+    updatedAt: "2022.12.31.(20:21:11)"
+}, {
+    comment_id: "7",
+    user_id: "test",
+    user_name: "테스트",
+    post_id: "2",
+    comment: "ㅋㅋㅋㅋㅋㅋㅋㅋ",
+    link_count: 0,
+    hate_count: 0,
+    createdAt: "2022.12.31.(20:20:11)",
+    updatedAt: "2022.12.31.(20:21:11)"
+}];
